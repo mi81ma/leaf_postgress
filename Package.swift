18f48e3e00fc99c799d382d72e2ccf029532cb51
mi81ma <mi81ma@gmail.com>
2018-12-17 11:01:01 +0800

    initial commit

--- Package.swift
+++ Package.swift
@@ -7,11 +7,18 @@ let package = Package(
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
 
+        // leaf dependency
+        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc"),
+
+//        // postgresql fluent
+//        .package(url: "https://gihub.com/vapor/fluent-postgresql.git", from: "1.0.0"),
+        .package(url: "https://github.com/vapor/postgresql.git", from: "1.0.0"),
+        
         // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
         .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0")
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentSQLite", "Vapor"]),
+    .target(name: "App", dependencies: ["FluentSQLite", "Vapor", "Leaf", "PostgreSQL"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
